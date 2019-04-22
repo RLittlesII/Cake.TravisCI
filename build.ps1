f8e25007739b0a4c7073632fa3f40e74a56ee942
Gary Ewan Park <gep13@gep13.co.uk>
2019-04-22 08:08:59 +0100

    (build) Bump version of Cake used to perform build
    - Since you are using the "latest" version fo Cake.Recipe
    - Have to use 0.32.1 for doing the build

--- build.ps1
+++ build.ps1
@@ -31,7 +31,7 @@ Param(
     [string[]]$ScriptArgs
 )
 
-$CakeVersion = "0.22.2"
+$CakeVersion = "0.32.1"
 $DotNetChannel = "preview";
 $DotNetVersion = "2.1.101";
 $DotNetInstallerUri = "https://dot.net/v1/dotnet-install.ps1";
